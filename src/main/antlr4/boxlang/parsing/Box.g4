478a4b24762cb26dc860160d11d01cfd69cec749
Takumi Bolte <takumib@gmail.com>
2015-02-21 19:37:27 -0500

    updating grammar

--- src/main/antlr4/boxlang/parsing/Box.g4
+++ src/main/antlr4/boxlang/parsing/Box.g4
@@ -1,3 +1,64 @@
 grammar Box;
 
-r : 'hello';
+file : expr EOF;
+
+stmt
+    : 'print' expr
+    |
+    ;
+
+decl
+    : 'let' Identifier ':' type ('=' expr)?
+    ;
+
+type
+    : 'int'
+    | 'float'
+    | 'bool'
+    | 'char'
+    | 'string'
+    ;
+
+expr
+    : expr op=('*' | '/') expr               #InfixExp
+    | expr op=('+' | '-') expr               #InfixExpr
+    | expr op=('<' | '>' | '<=' | '>=') expr #RelationalExpr
+    | expr op=('==' | '!=') expr             #RelationalExpr
+    | primary                                #PrimaryExpr
+    ;
+
+primary
+    : Integer
+    | Boolean
+    | Float
+    | Identifier
+    ;
+
+Integer
+    : DIGIT+
+    ;
+
+Boolean
+    : 'true'
+    | 'false'
+    ;
+
+Float
+    : DIGIT+ '.' DIGIT+
+    ;
+
+Identifier
+    : LETTER+ (DIGIT | LETTER)*
+    ;
+
+fragment
+DIGIT
+    : [0-9]
+    ;
+
+fragment
+LETTER
+    : [a-zA-Z]
+    ;
+
+WS : [ \t\r\n] -> skip;
--- src/main/java/boxlang/BoxCompiler.java
+++ src/main/java/boxlang/BoxCompiler.java
@@ -1,7 +1,33 @@
 package boxlang;
 
+import boxlang.parsing.BoxLexer;
+import boxlang.parsing.BoxParser;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.io.IOException;
+
 /**
  * Created by bolte on 2/21/15.
  */
 public class BoxCompiler {
+    public static void main(String[] args) {
+        try {
+
+            ANTLRInputStream input = new ANTLRFileStream(args[0]);
+            BoxLexer lexer = new BoxLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            BoxParser parser = new BoxParser(tokens);
+            ParseTree tree = parser.file();
+            ParseTreeWalker walker = new ParseTreeWalker();
+
+            //System.out.println(tree.getText());
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
